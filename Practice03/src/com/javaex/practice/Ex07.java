4ef039c28780a1dc64950931430d7ab7d867aea4
User <User@DESKTOP-BOSJB4J>
2024-03-19 02:44:45 +0900

    pra3

---
+++ Practice03/src/com/javaex/practice/Ex07.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex07 {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자를 입력하세요: ");
+        int num = scanner.nextInt();
+        
+   
+        
+        scanner.close();
+    }
+
+}
---
+++ Practice03/src/com/javaex/practice/Ex16.java
@@ -0,0 +1,28 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex16 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("숫자를 입력하세요: ");
+        int number = scanner.nextInt();
+
+        int count = 0;
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            if (i % 5 == 0) {
+                count++;
+                sum += i;
+            }
+        }
+        
+        System.out.println("5의배수의 개수: " + count);
+        System.out.println("5의배수의 합: " + sum);
+
+        scanner.close();
+    }
+
+}
