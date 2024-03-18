006a4155c10ecb64a2997163273caea7aef9d1c0
User <User@DESKTOP-BOSJB4J>
2024-03-19 02:38:44 +0900

    pra2

---
+++ Practice02/src/com/javaex/practice/Ex15.java
@@ -0,0 +1,53 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("출력되는 내용을 입력하세요");
+        System.out.print("기호:");
+        char o = scanner.next().charAt(0);
+
+        System.out.print("숫자1:");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("숫자2:");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+
+        switch (o) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("계산할 수 없습니다.");
+                    return;
+                } else {
+                    result = num1 / num2;
+                    break;
+                }
+            default:
+                System.out.println("계산할 수 없는 기호입니다");
+                return;
+        }
+
+        System.out.println("결과: " + result);
+
+        scanner.close();
+    }
+	
+
+}
